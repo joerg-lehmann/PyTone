2367e982467d28c92c0fa8a95c26fee9130a01e8
Jörg Lehmann <joerg-lehmann@users.noreply.github.com>
2005-07-17 21:27:47 +0000

    remove hack now that threadedplugins works correctly

--- src/plugin.py
+++ src/plugin.py
@@ -38,7 +38,7 @@ class plugin:
 class threadedplugin(service.service, plugin):
 
     def __init__(self, channel, config, mainscreen):
-        service.service.__init__(self, self.__class__.__name__, daemonize=True)
+        service.service.__init__(self, self.__class__.__name__)
         # as independent thread, we have to use our own channel which has
         # been created by the service constructor
         plugin.__init__(self, self.channel, config, mainscreen)
