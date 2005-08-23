261bc9184c169ddcdc92c0140bf642d61716c103
Jörg Lehmann <joerg-lehmann@users.noreply.github.com>
2005-08-23 16:54:20 +0000

    add gettext.sh helper script

---
+++ gettext.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+POTFILE=locale/PyTone.pot
+LOCALES="de it pl fr"
+
+pygettext -o $POTFILE src/*.py src/services/*.py src/services/songdbs/*.py src/services/players/*.py
+for locale in $LOCALES; do
+  echo Processing locale $locale...
+  localedir=locale/$locale/LC_MESSAGES
+  msgmerge $localedir/PyTone.po $POTFILE -o $localedir/PyTone.po.new
+  mv $localedir/PyTone.po $localedir/PyTone.po.old
+  mv $localedir/PyTone.po.new $localedir/PyTone.po
+done
