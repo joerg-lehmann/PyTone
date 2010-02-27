389045922f1897ca65cf3494141b3b3a454d8683
Jörg Lehmann <joerg-lehmann@users.noreply.github.com>
2010-02-27 14:38:22 +0000

    untested code for eyeD3 reader

--- src/metadata.py
+++ src/metadata.py
@@ -387,7 +387,7 @@ def read_mp3_eyeD3_metadata(md, path):
             if length:
                 md.length = length
         md.lyrics = u"".join(mp3info.getLyrics())
-        md.comments = u"".join(mp3info.getComments())
+        md.comments = u"".join(map(lambda c: c.render(), mp3info.getComments()))
         md.bpm = mp3info.getBPM()
 
         for rva2frame in mp3info.frames["RVA2"]:
