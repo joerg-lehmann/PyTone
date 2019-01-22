b84e2f9843c3f9258acc1da7e4d3b581df4e0e98
Jörg Lehmann <joerg-lehmann@users.noreply.github.com>
2019-01-22 21:52:02 +0100

    remove fallback for encodings which are not needed in Python 3

--- src/encoding.py
+++ src/encoding.py
@@ -1,25 +1,11 @@
 import sys
 import locale
 
-_fallbacklocalecharset = "iso-8859-1"
-
-try:
-    # works only in python > 2.3
-    _localecharset = locale.getpreferredencoding()
-except:
-    try:
-        _localecharset = locale.getdefaultlocale()[1]
-    except:
-        try:
-            _localecharset = sys.getdefaultencoding()
-        except:
-            _localecharset = _fallbacklocalecharset
-if _localecharset in [None, 'ascii', 'ANSI_X3.4-1968']:
-    _localecharset = _fallbacklocalecharset
+_localecharset = locale.getpreferredencoding()
 
 _fs_encoding = sys.getfilesystemencoding()
-if _fs_encoding in [None, 'ascii', 'ANSI_X3.4-1968']:
-    _fs_encoding = _fallbacklocalecharset
+#if _fs_encoding in [None, 'ascii', 'ANSI_X3.4-1968']:
+
 
 # exported functions
 
